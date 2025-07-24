cb52e717c7f1b6510d9b1435d1396f11a0b89e78
Nicola Rocchi <nicolarocchi97@gmail.com>
2025-07-24 15:55:32 +0200

    Register

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { supabase } from "./supporto/supabaseClient";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+
+
 
 export default function LoginForm() {
+
+    const [showPassword, setShowPassword] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
@@ -29,33 +34,39 @@ export default function LoginForm() {
         <form onSubmit={handleLogin} className="max-w-sm mx-auto mt-8 p-6 bg-white shadow-md rounded-xl space-y-4">
             <h2 className="text-xl font-semibold">Login</h2>
 
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-                className="w-full border p-2 rounded "
-            />
+            <div className="relative">
 
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-                className="w-full border p-2 rounded"
-            />
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                    className="w-full border p-2 rounded "
+                />
 
-            <button
-                type="submit"
-                disabled={loading}
-                className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded disabled:opacity-50"
-            >
-                {loading ? "Caricamento..." : "Login"}
-            </button>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                    className="w-full border p-2 rounded"
+                />
+
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded disabled:opacity-50"
+                >
+                    {loading ? "Caricamento..." : "Login"}
+                </button>
+            </div>
 
             {error && <p className="text-red-600">{error}</p>}
+            <p className="text-center">
+                Hai già un account? <Link className="text-blue-600 underline" to="/register">Registrati</Link>
+            </p>
         </form>
     );
 }
